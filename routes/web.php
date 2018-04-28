44a35928b970ed633313a08a2b63e4c32688f638
wahyuadepratama <wahyuadepratam4@gmail.com>
2018-04-28 07:42:17 +0700

    laravel added - dont update composer

---
+++ routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+// Route::get('/', function () { return view('home'); });
+//
+// Route::get('/input', function () { return view('input'); });
+// Route::post('/input', 'DataController@create');
