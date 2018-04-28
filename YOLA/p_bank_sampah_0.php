8a7c621bd07aab5f7f30f65428e2b4c80c7960a6
nabang97 <parawitayolanda@gmail.com>
2018-04-28 08:36:09 +0700

    add layouts

---
+++ YOLA/p_bank_sampah_0.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+  <?php include_once 'layouts/head.php'; ?>
+  <body>
+    <?php include_once 'layouts/header.php'; ?>
+
+      <div class="container" style="padding-top:30px;">
+        <div class="grid-bank-sampah-0">
+          <div class="list_sampah">
+            <div class="box-list">
+              <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="box-list">
+              <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                </div>
+            </div>
+            <div class="box-list">
+              <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="box-list">
+              <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div class="jenis_sampah">
+            <div class="panel">
+              <div class="panel-header">
+                <span>Jenis Sampah untuk Transaksi di Bank Sampah</span>
+              </div>
+              <div class="panel-body-bank">
+                <div class="box-content">
+                  <div class="foto">
+                    foto
+                  </div>
+                  <div class="detail-desk">
+                    <small>Some quick example text to build on the card title and make up the bulk of the card's content.</small>
+                  </div>
+                </div>
+                <div class="box-content">
+                  <div class="foto">
+                    foto
+                  </div>
+                  <div class="detail-desk">
+                    <small>heeyy</small>
+                  </div>
+                </div>
+                <div class="box-content">
+                  <div class="foto">
+                    foto
+                  </div>
+                  <div class="detail-desk">
+                    <small>heeyy</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- END GRID LOCATION -->
+      </div>
+        <!-- END CONTAINER -->
+        <?php include_once 'layouts/footer.php'; ?>
+      <script src="assets/js/map.js" charset="utf-8"></script>
+      <script async defer
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCaKLihHUYfMPMCdAgzPlsXamo9BthmImk&callback=initMap">
+      </script>
+  </body>
+</html>
